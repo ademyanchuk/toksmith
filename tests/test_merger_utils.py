08449f35df5ae87e298647721a79ae69afc5751a
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2025-06-04 14:29:39 +0200

    Add test cases for _get_pair_stats

--- tests/test_merger_utils.py
+++ tests/test_merger_utils.py
@@ -9,6 +9,13 @@ from toksmith.merger import _get_pair_stats
   [
     # 1) Empty pretoken counter
     (dict(), (dict(), dict())),
+    # 2) Single unit pretoken
+    ({(42,): 2}, (dict(), dict())),
+    # 3) Happy case
+    (
+      {(1, 2, 3): 2, (2, 3): 3},
+      ({(1, 2): 2, (2, 3): 5}, {(1, 2): {(1, 2, 3)}, (2, 3): {(1, 2, 3), (2, 3)}}),
+    ),
   ],
 )
 def test_get_pair_stats(pretoken_count, expected):
